2968d841a6d961639fa1a57b8d02fd5c8d2320f0
KeBiRo8 <nakamura8ebi@gmail.com>
2023-10-27 08:19:31 +0900

    blog_postdetail

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    public function getPaginateByLimit(int $limit_count = 2)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'desc')->paginate($limit_count);
+    //orderby('並び替える基準となるカラム名','desc'降順,'asc'昇順)
+    }
 }
+
