35d626f61b8305de1be73142bcd23b913d643588
KeBiRo8 <nakamura8ebi@gmail.com>
2023-11-03 12:43:19 +0900

    ブログ投稿編集

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use Illuminate\HTTP\Request;
 
 class Postcontroller extends Controller
 {
@@ -17,7 +18,7 @@ class Postcontroller extends Controller
     public function index(Post $post)
     {
     //return view('posts.index')->with(['posts' => $post->getByLimit()]);
-    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     //getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
     public function show(Post $post)
@@ -40,9 +41,22 @@ class Postcontroller extends Controller
         //post[~]というもののみを取得し$inputに保存
         $post->fill($input)->save();
         //$postのプロパティを受け取ったデータで上書き
-        return redirect('post/'.$post->id);
+        return redirect('/post/'.$post->id);
         //$post->idを引数に入れることで、作製した投稿の詳細ページへ画面を遷移させることができる
     }
+    public function edit(Post $post)
+    //引数はPostだが、URLから渡ってきたidを用いてインスタンス化した$post
+    {
+        return view('posts.edit')->with(['post'=>$post]);
+        //posts/editのビュー画面に$postのidをもつ投稿内容を表示させる
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
 
+        return redirect('/posts/' . $post->id);
+    }
 
 }
